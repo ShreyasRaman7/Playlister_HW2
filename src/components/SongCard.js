1bd3f5d103986b63740e0608cafed15bd991f20d
ShreyasRaman7 <71574041+ShreyasRaman7@users.noreply.github.com>
2022-09-20 15:40:38 -0400

    got song hyperlinks to work

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -76,8 +76,10 @@ export default class SongCard extends React.Component {
                 onDragLeave={this.handleDragLeave}
                 onDrop={this.handleDrop}
                 draggable="true"
+                
             >
-                {num}. {song.title} by {song.artist}
+                
+                {num}. <a href={"https://www.youtube.com/watch?v=" +song.youTubeId }>  {song.title} by {song.artist}</a>
             </div>
         )
     }
